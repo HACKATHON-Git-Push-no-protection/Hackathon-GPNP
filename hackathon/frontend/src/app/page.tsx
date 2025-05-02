9b1c90ab7e4bd32a297e7a2b9c83771bdb16f7d7
Ricardo <ricardo_fernandes21@hotmail.com>
2025-05-02 23:54:38 +0200

    Add : FastApi

--- hackathon/frontend/src/app/page.tsx
+++ hackathon/frontend/src/app/page.tsx
@@ -1,8 +1,25 @@
+
+"use client";
 import Image from "next/image";
+import { useEffect, useState } from 'react';
+
+
 
 export default function Home() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/v1/')
+      .then(res => res.json())
+      .then(data => setMessage(data.message))
+      .catch(err => console.error('Error:', err));
+  
+  }, []);
+
+  console.log(message)
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div>Response from FastAPI: {message}</div>;
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
