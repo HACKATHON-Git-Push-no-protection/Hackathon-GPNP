4a73daec5a7672bd446326db953b4af4126eeb37
Ricardo <ricardo_fernandes21@hotmail.com>
2025-05-03 01:45:24 +0200

    Update main.py

--- hackathon/backend/main.py
+++ hackathon/backend/main.py
@@ -23,8 +23,32 @@ async def root():
     return {"message": "API is running"}
 
 
-@router.post("/predict")
-async def predict(data):
-    return {"prediction"}
+
+all_features = [
+    'age', 'smell_intensity', 'sleep_hours', 'timestamp', 'gender', 'height_cm',
+    'weight_kg', 'hydration_level', 'activity_level', 'meds_affecting_gut',
+    'fiber_grams', 'fat_grams', 'spiciness', 'weekly_greasy_meals', 'dairy_freq',
+    'processed_servings', 'fv_servings', 'toilet_method', 'stool_consistency',
+    'stool_color', 'weekly_bms', 'caffeinated_beverages_per_day'
+]
+
+
+@router.post("/predict",response_model=dict)
+async def predict(req):
+
+    answers = req.answers
+    feature_vector = []
+    for feat in all_features:
+        val = answers.get(feat, 0)
+        try:
+            val = float(val)
+        except:
+            pass 
+        feature_vector.append(val)
+
+    pred = model.predict([feature_vector])
+    return {"prediction ": pred.tolist()}
+
+
 
 app.include_router(router)
